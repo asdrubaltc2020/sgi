5a7e90001d0258bf81363a91994609f685eafb38
Asdrubal Torres <atorres@bsnnet.com>
2020-02-08 18:42:14 -0500

    Updated symfony framework from 2.7.3 to 2.8.5

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,7 +18,6 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new Briooz\TaskBundle\BrioozTaskBundle(),
-            new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
             new Knp\Bundle\SnappyBundle\KnpSnappyBundle(),
             new WhiteOctober\TCPDFBundle\WhiteOctoberTCPDFBundle(),
             new Briooz\VentasBundle\BrioozVentasBundle(),
